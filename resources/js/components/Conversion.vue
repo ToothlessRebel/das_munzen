cdd085767278817afe46ad06946aeb0847f1c5a7
Marshall Davis <marshall@sourcetoad.com>
2021-10-19 15:07:01 -0400

    Show error messages

--- resources/js/components/Conversion.vue
+++ resources/js/components/Conversion.vue
@@ -9,35 +9,41 @@
                     How much have you got? (In USD)
                     <input type="number" step="0.01" min="0" name="value" class="rounded-2xl" v-model="total"/>
                 </label>
-                <button class="rounded-2xl bg-green-600 border-green-600 text-white p-3" @click="convert">Cha-ching!</button>
+                <button class="rounded-2xl bg-green-600 border-green-600 text-white p-3" @click="convert">Cha-ching!
+                </button>
             </div>
-            <div v-if="errors.length" class="error text-red-600">
-                Oh no, there were errors!
+            <div v-if="errors !== undefined" class="error text-red-600">
+                <div class="error" v-for="error in Object.entries(errors)">
+                    <div v-for="message in error[1]">{{message}}</div>
+                </div>
             </div>
-            <coinage v-if="coins !== undefined" :coins="coins" />
+            <coinage v-if="coins !== undefined" :coins="coins"/>
         </div>
     </div>
 </template>
 
 <script>
 import Coinage from "./Coinage";
+
 export default {
     name: "Conversion",
     components: {Coinage},
     data() {
         return {
             total: null,
-            errors: [],
-            coins: undefined
+            errors: undefined,
+            coins: undefined,
         }
     },
     methods: {
         convert() {
             axios.get('/api/convert/USD?value=' + this.total)
-            .then(response => this.coins = response.data)
-            .catch(error => console.error(error))
-        }
-    }
+                .then(response => this.coins = response.data)
+                .catch(error => {
+                    this.errors = error.response.data.errors
+                })
+        },
+    },
 }
 </script>
 
